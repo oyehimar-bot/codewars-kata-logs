4d858e7b3b3c28e5f28d44cbfa10d454955901e3
Oyelade Emmanuel <oyehimar@gmail.com>
2026-06-18 12:41:50 +0100

    Add solution - CodeHub

---
+++ 7-kyu/leap-years/leap-years.go
@@ -0,0 +1,15 @@
+package kata
+​
+func IsLeapYear(year int) bool {
+  switch {
+    case year % 400 == 0:
+      return true
+    case year % 100 == 0:
+      return false
+    case year % 4 == 0:
+      return true
+    default:
+      return false
+  }
+}
+  
